9dd97b4fe1168b6ec481066ad9684f6ac0283076
郭振涛 <gztcode@gmail.com>
2020-11-23 14:30:22 +0800

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,71 +1,12 @@
 #!/usr/bin/bash
-
- 
-
-echo > /var/log/syslog
-
- 
-
-echo > /var/log/messages
-
- 
-
-echo > /var/log/xferlog
-
- 
-
-echo > /usr/local/nginx/logs/access.log
-
- 
-
-echo > /home/wwwlogs/access.log
-
- 
-
-#echo > /var/log/httpd/access_log
-
- 
-
-#echo > /var/log/httpd/error_log
-
- 
-
-echo > /var/log/secure
-
- 
-
-echo > /var/log/auth.log
-
- 
-
-echo > /var/log/user.log
-
- 
-
-echo > /var/log/wtmp
-
- 
-
-echo > /var/log/lastlog
-
- 
-
-echo > /var/log/btmp
-
- 
-
-echo > /var/run/utmp
-
- 
-
-echo > .bash_history
-
- 
-
-echo > .mysql_history
-
-
 history -c
-
- 
-chmod +x clear.sh
+cat /dev/null > /var/log/boot.log
+cat /dev/null > /var/log/cron
+cat /dev/null > /var/log/dmesg
+cat /dev/null > /var/log/firewalld
+cat /dev/null > /var/log/maillog
+cat /dev/null > /var/log/messages
+cat /dev/null > /var/log/spooler
+cat /dev/null > /var/log/syslog
+cat /dev/null > /var/log/tallylog
+cat /dev/null > /var/log/yum.log
